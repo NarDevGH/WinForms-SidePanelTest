e2c6f340cb2716c2d68077d179a85eb635cbcb97
NarMaroth <bautista.mateo@outlook.com>
2022-11-15 00:53:46 -0300

    first commit

---
+++ test/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace test
+{
+    class Class1 : Button
+    {
+        private bool gigante = true;
+
+        [Description("Una descripcion re copada")]
+        [Category("CosasGigante")]
+        [DefaultValue(true)] 
+        public bool Gigante {
+            get { return gigante; }
+            set { gigante = value; }
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            if (gigante)
+                this.Size = new System.Drawing.Size(150, 150);
+            base.OnMouseEnter(e);
+        }
+    }
+}
---
+++ test/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private int num = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button txtBox = new Button();
+            txtBox.Name = "Panel:" + num++;
+            txtBox.Location = new System.Drawing.Point(13,200);
+            txtBox.Size = new System.Drawing.Size(10, 50);
+            this.panel1.Controls.Add(txtBox);
+            txtBox.Parent = this.panel1;
+            txtBox.Dock = DockStyle.Top;
+            txtBox.Text = txtBox.Name+", "+txtBox.Parent.Name;
+        }
+    }
+}
